95c8e61d1388269e7ec1bf893632ab2b6ac4046b
YUNUS EMRE MERMER <yunusemremermer57@gmail.com>
2022-09-26 15:10:46 +0300

    26.09.22

--- app.js
+++ app.js
@@ -1,11 +1,40 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import Post from "./models/Post.js"
 
 const app = express();
 
-app.get("/", (req, res) => {
-    const blog = { id: 1, title: "Blog title", description: "Blog description" }
-    res.send(blog)
-})
+// Connect Db
+mongoose.connect('mongodb://localhost/cleanblog-test-db');
+
+//Middlewares
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//template engine
+app.set('view engine', 'ejs');
+
+app.get('/', async (req, res) => {
+  
+  const posts = await Post.find({});
+  res.render('index', {
+    posts,
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+
+app.post('/add_post', async (req, res) => {
+  await Post.create(req.body)
+  res.redirect('/');
+});
 
 const port = 3000;
 
